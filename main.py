6dc19505629fdbe687c7d5dbd83bdd9863b75048
kevinelong <kevinelong@gmail.com>
2022-01-10 07:50:53 -0800

    format

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import curses
 import sys
 
+
 class CursesInterface:
     data = {
-        8:"BACKSPACE",
-        9:"\t",
-        10:"\r\n",
-        27:"ESCAPE",
-        258:"DOWN", 
-        259:"UP",
-        260:"LEFT",
-        261:"RIGHT",
-        262:"HOME",
-        263:"DELETE",
-        331:"INSERT",
-        338:"PAGE_DOWN",
-        339:"PAGE_UP",
-        353:"BACK_TAB",
-        360:"END",
+        8: "BACKSPACE",
+        9: "\t",
+        10: "\r\n",
+        27: "ESCAPE",
+        258: "DOWN",
+        259: "UP",
+        260: "LEFT",
+        261: "RIGHT",
+        262: "HOME",
+        263: "DELETE",
+        331: "INSERT",
+        338: "PAGE_DOWN",
+        339: "PAGE_UP",
+        353: "BACK_TAB",
+        360: "END",
     }
 
     def __init__(self):
-        self.screen = curses.initscr() 
-        curses.noecho() 
+        self.screen = curses.initscr()
+        curses.noecho()
         curses.cbreak()
         self.screen.keypad(True)
 
@@ -34,11 +35,12 @@ class CursesInterface:
             return chr(k)
         return f"{k}"
 
-    def restore(self):          
+    def restore(self):
         curses.nocbreak()
         self.screen.keypad(0)
         curses.echo()
         curses.endwin()
+
 
 ci = CursesInterface()
 quitting = False
